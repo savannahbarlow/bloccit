2af74a0be9328b5e3873c25306fc40112fa2ad49
Savannah Barlow <barlow.savannah@gmail.com>
2016-05-12 16:31:33 -0700

    Assignment 30 Rails Seeding Data: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,19 @@ posts = Post.all
 100.times do
   Comment.create!(
   post: posts.sample,
-  body: RadnomData.random_paragraph
+  body: RandomData.random_paragraph
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "The Unique Post Title", body: "The unique post body.", id: 999)
+puts "#{Post.count}"
+
+puts "#{Comment.count}"
+Comment.find_or_create_by(body: "The unique comment body.", post_id: 999)
+puts "#{Comment.count}"
+
+
 puts "Seed Finished"
-puts "#{post.count} posts created"
+puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
