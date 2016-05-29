0f9eba7fc3313838231d49687ddd8829e37ef26d
Savannah Barlow <barlow.savannah@gmail.com>
2016-05-29 12:03:48 -0700

    Checkpoint 34 Rails Topics:completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,33 @@
 require 'random_data'
 
+# Create Topics
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
+# Create Posts
 50.times do
   Post.create!(
+  topic: topics.sample,
   title:  RandomData.random_sentence,
   body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
 
+# Create Comments
 100.times do
   Comment.create!(
   post: posts.sample,
-  body: RadnomData.random_paragraph
+  body: RandomData.random_paragraph
   )
 end
 
 puts "Seed Finished"
-puts "#{post.count} posts created"
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+  let(:title) { RandomData.random_sentence }
+  let(:body) { RandomData.random_paragraph }
+  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:post) { topic.posts.create!(title: title, body: body) }
 
-  let (:post) {Post.create!(title: "New Post Title", body: "New Post Body")}
+  it { is_expected.to belong_to(:topic) }
 
   describe "attributes" do
     it "has title and body attributes" do
-      expect(post).to have_attributes(title:"New Post Title", body: "New Post Body")
+      expect(post).to have_attributes(title: title, body: body)
     end
   end
 end
