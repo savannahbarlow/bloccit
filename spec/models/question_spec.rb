9887cf3d2cc3fc99a5ca3b55d0af321df8b42680
Savannah Barlow <barlow.savannah@gmail.com>
2016-06-05 09:10:47 -0700

    Checkpoint 39 completed

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let (:post) {Post.create!(title: "New Post Title", body: "New Post Body")}
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+  let(:title) { RandomData.random_sentence }
+  let(:body) { RandomData.random_paragraph }
+  let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld")}
+  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:post) { topic.posts.create!(title: title, body: body, user: user) }
 
   describe "attributes" do
     it "has title and body attributes" do
-      expect(post).to have_attributes(title:"New Post Title", body: "New Post Body")
+      expect(post).to have_attributes(title: title, body: body, user: user)
     end
   end
 end
