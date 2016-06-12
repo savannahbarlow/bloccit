8184e907879d8528503f8757fcbc9bb941aa0f9d
Savannah Barlow <barlow.savannah@gmail.com>
2016-06-11 18:05:24 -0700

    Checkpoint 40 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
+  before_action :authorize_user, except: [:show, :new, :create]
 
   def show
     @post = Post.find(params[:id])
@@ -11,11 +12,9 @@ class PostsController < ApplicationController
   end
 
   def create
-
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.build(post_params)
     @post.user = current_user
-
 
     if @post.save
       flash[:notice] = "Post was saved successfully."
@@ -58,5 +57,12 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+  def authorize_user
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin?
+      flash[:alert] = "You must be admin to do that."
+      redirect_to [post.topic, post]
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,10 +38,22 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'barlow.savannah@gmail.com',
-  password: 'helloworld'
+#Create an admin user
+
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+
+#Create a member
+
+member = User.create!(
+name: 'Member User',
+email: 'member@example.com',
+password: 'helloworld'
 )
 
 puts "Seed Finished"
