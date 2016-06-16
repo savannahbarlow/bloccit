93a789842ac2a2fba5ef6264a271eb565c0884d0
Savannah Barlow <barlow.savannah@gmail.com>
2016-06-15 19:22:59 -0700

    Assignment 43 complete

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -60,6 +60,19 @@ RSpec.describe Post, type: :model do
         end
       end
 
+      describe "#create_vote" do
+
+        it "calls #create_vote when a post is created" do
+          post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+          expect(post).to receive(:create_vote)
+          post.save
+        end
+
+        it "associates the vote with the owner of the post" do
+          expect(post.votes.first.user).to eq(post.user)
+        end
+      end
+
       describe "#update_rank" do
 
        it "calculates the correct rank" do
