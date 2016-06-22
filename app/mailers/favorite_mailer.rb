a8bea916e24f856da0b7e986588ab6734a4a5a05
Savannah Barlow <barlow.savannah@gmail.com>
2016-06-22 12:14:50 -0700

    Assignment 44 complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "You have a new comment on #{post.title}.")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@salty-refuge-91119.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@salty-refuge-91119.herokuapp.com>"
+    headers["References"] = "<post/#{post.id}@salty-refuge-91119.herokuapp.com>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following, #{post.title}!")
+  end
 end
