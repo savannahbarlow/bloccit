c35e2044d98d34d9afafa46ff78c41011e889ef3
Savannah Barlow <barlow.savannah@gmail.com>
2016-06-26 17:11:12 -0700

    Checkpoint 48 in progress v1

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,10 @@ end
 
   root 'welcome#index'
 
+    namespace :api do
+      namespace :v1 do
+        resources :users, only: [:index, :show]
+        resources :topics, only: [:index, :show]
+      end
+    end
 end
